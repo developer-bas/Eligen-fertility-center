c78d77687f547268b68b22c2dcbb413d5f2f80d8
Sebas <cruzguzmaneric@gmail.com>
2020-09-19 19:49:16 -0500

    send email capability

---
+++ enviar.php
@@ -0,0 +1,19 @@
+<?php
+
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$telefono = $_POST['telefono'];
+$mensaje = $_POST['mensaje'];
+
+$destinatario ="cruzguzmans@gmail.com";
+$asunto  = "Contacto desde la pagina WEB ";
+
+$carta = "De : $nombre \n";
+$carta .= "Correo : $correo \n";
+$carta .= "Telefono : $telefono  \n";
+$carta .=  "Mensaje: $mensaje";
+
+mail($destinatario,$asunto,$carta);
+header('Location:index.html')
+
+?>
